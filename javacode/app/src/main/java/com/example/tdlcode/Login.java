aceea1c0f76cc0053445c0a5559253d96be6d61b
Necosama <tug65044@temple.edu>
2021-11-10 15:38:11 -0500

    Login Frame Created: Create a frame for logging in and let user to switch to registration frame.

--- javacode/app/src/main/java/com/example/tdlcode/Login.java
+++ javacode/app/src/main/java/com/example/tdlcode/Login.java
@@ -7,21 +7,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        Button registerBtn = (Button)findViewById(R.id.rigbtn);
+        setContentView(R.layout.activity_login);
+
+        //
+        Button registerBtn = (Button)findViewById(R.id.loginBtn);
         registerBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,Account.class);
+                Intent intent = new Intent(Login.this,Account.class);
                 startActivity(intent);
             }
         });
-
     }
 }
