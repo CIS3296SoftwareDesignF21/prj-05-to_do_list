8a4470e009b8ede747a866be82e25fd4135289cd
Necosama <tug65044@temple.edu>
2021-11-16 23:01:53 -0500

    Update for Logging function: Now, it's able to check whether there is an existing user, if so, go to next page, if not, stay.

--- javacode/app/src/main/java/com/example/tdlcode/Login.java
+++ javacode/app/src/main/java/com/example/tdlcode/Login.java
@@ -6,6 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
 
 public class Login extends AppCompatActivity {
 
@@ -14,14 +18,41 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        //
-        Button registerBtn = (Button)findViewById(R.id.loginBtn);
-        registerBtn.setOnClickListener(new View.OnClickListener(){
+        //let user to log in.
+        Button loginBtn = (Button)findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Login.this,Account.class);
+                //get user's name and password
+                TextView nametx = (TextView)findViewById(R.id.uname);
+                TextView passwordtx = (TextView)findViewById(R.id.upassword);
+                String name = nametx.getText().toString();
+                String password = passwordtx.getText().toString();
+                //check if there is an existing user, then allow him to log in
+                if("testname".equals(name) && "testpassword".equals(password)){
+                    Toast.makeText(Login.this,"Welcome Home",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(Login.this,Account.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(Login.this,"Incorrect name or password",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        //let user to register
+        Button regBtn = (Button)findViewById(R.id.regBtn);
+        regBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+
+                //show registration frame
+                Intent intent = new Intent(Login.this,MainActivity.class);
                 startActivity(intent);
             }
         });
     }
+
+    //function to check User information by getting string value from TextView
+
 }
