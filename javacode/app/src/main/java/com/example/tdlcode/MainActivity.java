8e19ac9b705f1fd3a24e3acc2e6aa1a21a1e4b4f
Abdullah_Arishi <55096042+Arishi2000@users.noreply.github.com>
2021-11-17 19:36:04 -0500

    Working on the list view function

--- javacode/app/src/main/java/com/example/tdlcode/MainActivity.java
+++ javacode/app/src/main/java/com/example/tdlcode/MainActivity.java
@@ -2,17 +2,46 @@ package com.example.tdlcode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView listView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //addItem(view);
+            }
+        });
+
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(itemsAdapter);
+        setUpListViewListener();
+
         
         Button registerBtn = (Button)findViewById(R.id.rigbtn);
         registerBtn.setOnClickListener(new View.OnClickListener(){
@@ -22,6 +51,30 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+    }
+
+    private void setUpListViewListener() {
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+
+            @Override
+            public boolean onItemLongClick(AdapterView<?> AdapterView, View view, int i, long l) {
+                // Get context for toast message
+                Context context = getApplicationContext();
+                //says that item been removed
+                Toast.makeText(context, "Item Removed", Toast.LENGTH_LONG).show();
+
+                items.remove(i);
+                // to make sure that adapter is up to date
+                itemsAdapter.notifyDataSetChanged();
+                return true;
+
+            }
+        });
+    }
+
+    private void addItem(View view){
+
 
     }
 }
