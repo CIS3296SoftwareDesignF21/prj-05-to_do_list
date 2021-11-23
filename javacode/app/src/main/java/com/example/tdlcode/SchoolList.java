8879ca32d62325d4156efe2fe64461615c171299
Haein Yu <tuk83021@temple.edu>
2021-11-23 16:30:23 -0500

    started working on School List. I created School List layout and haven't finished yet

---
+++ javacode/app/src/main/java/com/example/tdlcode/SchoolList.java
@@ -0,0 +1,13 @@
+package com.example.tdlcode;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SchoolList extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.school_list);
+    }
+}
